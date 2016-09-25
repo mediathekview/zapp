d4f083a5a7022f3a8d69b5bc0b7db1b3533d94b1
Christine Emrich <christine.emrich@web.de>
2016-09-25 12:22:22 +0200

    Rename fragment to match class name

--- app/src/main/java/de/christinecoenen/code/zapp/fragments/StreamPageFragment.java
+++ app/src/main/java/de/christinecoenen/code/zapp/fragments/StreamPageFragment.java
@@ -33,7 +33,7 @@ public class StreamPageFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View rootView = inflater.inflate(R.layout.fragment_channel_detail, container, false);
+		View rootView = inflater.inflate(R.layout.fragment_stream_page, container, false);
 		ButterKnife.bind(this, rootView);
 
 		Bundle args = getArguments();
