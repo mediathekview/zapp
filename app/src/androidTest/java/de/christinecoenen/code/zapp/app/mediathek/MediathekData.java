ef0fc5ec4c51d53d140b720920000d7cbdf10335
Christine Emrich <christine.emrich@web.de>
2019-05-29 21:12:16 +0200

    Add very basic activity tests

---
+++ app/src/androidTest/java/de/christinecoenen/code/zapp/app/mediathek/MediathekData.java
@@ -0,0 +1,16 @@
+package de.christinecoenen.code.zapp.app.mediathek;
+
+import de.christinecoenen.code.zapp.app.mediathek.model.MediathekShow;
+
+public class MediathekData {
+
+	public static MediathekShow getTestShow() {
+		MediathekShow mediathekShow = new MediathekShow();
+		mediathekShow.setId("test123");
+		mediathekShow.setChannel("ARD");
+		mediathekShow.setTitle("My Title");
+		mediathekShow.setVideoUrl("http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4");
+		return mediathekShow;
+	}
+
+}
