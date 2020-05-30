3b470589088a565d60ac39428e6959d1268c693b
Christine Emrich <christine.emrich@web.de>
2020-05-30 21:04:56 +0200

    Proof of concept for using Android 10 MediaStore

--- app/src/main/java/de/christinecoenen/code/zapp/app/mediathek/controller/downloads/DownloadController.java
+++ app/src/main/java/de/christinecoenen/code/zapp/app/mediathek/controller/downloads/DownloadController.java
@@ -1,11 +1,19 @@
 package de.christinecoenen.code.zapp.app.mediathek.controller.downloads;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.media.MediaScannerConnection;
 import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.provider.MediaStore;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
 
 import com.tonyodev.fetch2.Download;
 import com.tonyodev.fetch2.Error;
@@ -19,12 +27,14 @@ import com.tonyodev.fetch2core.DownloadBlock;
 
 import java.io.File;
 import java.util.List;
+import java.util.Set;
 
 import de.christinecoenen.code.zapp.app.mediathek.controller.downloads.exceptions.DownloadException;
 import de.christinecoenen.code.zapp.app.mediathek.controller.downloads.exceptions.WrongNetworkConditionException;
 import de.christinecoenen.code.zapp.app.mediathek.model.MediathekShow;
 import de.christinecoenen.code.zapp.app.mediathek.model.Quality;
 import de.christinecoenen.code.zapp.app.settings.repository.SettingsRepository;
+import timber.log.Timber;
 
 public class DownloadController implements FetchListener {
 
@@ -57,16 +67,43 @@ public class DownloadController implements FetchListener {
 		fetch.addListener(this);
 	}
 
+	@RequiresApi(api = Build.VERSION_CODES.Q)
 	public void startDownload(MediathekShow show, Quality quality) {
 		String downloadUrl = show.getVideoUrl(quality);
 		String fileName = show.getDownloadFileName(quality);
-		String filePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).getAbsolutePath()
-			+ "/zapp/" + fileName;
+
+		// TODO: is this the right directory?
+		// for lower than Android Q
+		String filePath = applicationContext.getExternalMediaDirs()[1].getAbsolutePath()
+			+ "/" + fileName;
+
+
+		// for Android Q and up
+		// https://medium.com/better-programming/all-you-need-to-know-about-scoped-storage-in-android-10-e621f40bc8b9
+
+		// available volumes (0 => primary, 1 => SD-Card)
+		Set<String> volumeNames = MediaStore.getExternalVolumeNames(applicationContext);
+		String[] volumeNamesArray = new String[volumeNames.size()];
+		volumeNames.toArray(volumeNamesArray);
+
+		Uri videoMediaStoreUri = MediaStore.Video.Media.getContentUri(volumeNamesArray[1]);
+
+		for (String volumeName : volumeNamesArray) {
+			Uri volumeUri = MediaStore.Video.Media.getContentUri(volumeName);
+			boolean isPrimary = volumeName.equals(MediaStore.VOLUME_EXTERNAL_PRIMARY);
+			Timber.d("volume: %s, isPrimary: %b, uri: %s", volumeName, isPrimary, volumeUri);
+		}
+
+		ContentValues videoContentValues = new ContentValues();
+		videoContentValues.put(MediaStore.Video.Media.DISPLAY_NAME, fileName);
+
+		ContentResolver resolver = applicationContext.getContentResolver();
+		Uri fileUri = resolver.insert(videoMediaStoreUri, videoContentValues);
 
 		Request request;
 		try {
 			// create request for android download manager
-			request = new Request(downloadUrl, filePath);
+			request = new Request(downloadUrl, fileUri);
 		} catch (Exception e) {
 			throw new DownloadException("Constructing download request failed.", e);
 		}
@@ -110,8 +147,9 @@ public class DownloadController implements FetchListener {
 	public void deleteDownloadsWithDeletedFiles() {
 		fetch.getDownloadsWithStatus(Status.COMPLETED, downloads -> {
 			for (Download download : downloads) {
+				// TODO: for Android Q check MediaStore for file existence
 				if (!new File(download.getFileUri().toString()).exists()) {
-					fetch.delete(download.getId());
+					//fetch.delete(download.getId());
 				}
 			}
 		});
@@ -132,7 +170,7 @@ public class DownloadController implements FetchListener {
 
 	private void rescanDownloadFile(@NonNull Download download) {
 		String filePath = download.getFileUri().getPath();
-		MediaScannerConnection.scanFile(applicationContext, new String[]{filePath}, new String[]{"video/*"}, null);
+		//MediaScannerConnection.scanFile(applicationContext, new String[]{filePath}, new String[]{"video/*"}, null);
 	}
 
 	@Override
