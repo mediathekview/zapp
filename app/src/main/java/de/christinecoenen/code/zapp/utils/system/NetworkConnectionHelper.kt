1ed40b8940c3b4815a6401191411c7291cf485d5
Christine Emrich <christine.emrich@web.de>
2020-12-27 12:04:42 +0100

    Convert NetworkConnectionHelper to kotlin

---
+++ app/src/main/java/de/christinecoenen/code/zapp/utils/system/NetworkConnectionHelper.kt
@@ -0,0 +1,49 @@
+package de.christinecoenen.code.zapp.utils.system
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import androidx.core.net.ConnectivityManagerCompat
+import java.lang.ref.WeakReference
+
+class NetworkConnectionHelper(context: Context) {
+
+	private val networkReceiver = NetworkReceiver()
+	private val contextReference: WeakReference<Context> = WeakReference(context)
+	private var listener: Listener? = null
+
+	fun startListenForNetworkChanges(listener: Listener?) {
+		this.listener = listener
+
+		val filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+		contextReference.get()?.registerReceiver(networkReceiver, filter)
+	}
+
+	fun endListenForNetworkChanges() {
+		listener = null
+		contextReference.get()?.unregisterReceiver(networkReceiver)
+	}
+
+	val isConnectedToUnmeteredNetwork: Boolean
+		get() {
+			val context = contextReference.get() ?: return false
+
+			val connectionManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+			return !ConnectivityManagerCompat.isActiveNetworkMetered(connectionManager)
+		}
+
+
+	private inner class NetworkReceiver : BroadcastReceiver() {
+
+		override fun onReceive(context: Context, intent: Intent) {
+			listener?.onNetworkChanged()
+		}
+
+	}
+
+	interface Listener {
+		fun onNetworkChanged()
+	}
+}
