32537561522c45be60221e712e3682c3aabb665e
Christine Emrich <christine.emrich@web.de>
2021-07-06 17:59:45 +0200

    Remove unused import

--- app/src/main/java/de/christinecoenen/code/zapp/app/livestream/ui/list/adapter/ChannelListAdapter.kt
+++ app/src/main/java/de/christinecoenen/code/zapp/app/livestream/ui/list/adapter/ChannelListAdapter.kt
@@ -1,6 +1,5 @@
 package de.christinecoenen.code.zapp.app.livestream.ui.list.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
