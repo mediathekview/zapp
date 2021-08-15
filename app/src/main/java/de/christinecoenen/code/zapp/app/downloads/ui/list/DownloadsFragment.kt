b5385b2e7cac1cd028af121a8e928489d3108f46
Christine Coenen <code.coenen@gmail.com>
2021-08-15 16:09:29 +0200

    Display message when no downloads are available

--- app/src/main/java/de/christinecoenen/code/zapp/app/downloads/ui/list/DownloadsFragment.kt
+++ app/src/main/java/de/christinecoenen/code/zapp/app/downloads/ui/list/DownloadsFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.RecyclerView
 import de.christinecoenen.code.zapp.app.downloads.ui.list.adapter.DownloadListAdapter
 import de.christinecoenen.code.zapp.app.mediathek.ui.detail.MediathekDetailActivity
 import de.christinecoenen.code.zapp.databinding.DownloadsFragmentBinding
@@ -23,18 +25,31 @@ class DownloadsFragment : Fragment(), DownloadListAdapter.Listener {
 	private val binding: DownloadsFragmentBinding get() = _binding!!
 
 	private val viewModel: DownloadsViewModel by viewModel()
+	private lateinit var downloadAdapter: DownloadListAdapter
 
-	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
 		_binding = DownloadsFragmentBinding.inflate(inflater, container, false)
 
-		val downloadAdapter = DownloadListAdapter(this, viewModel)
+		downloadAdapter = DownloadListAdapter(this, viewModel)
 		binding.list.adapter = downloadAdapter
+
+		downloadAdapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+			override fun onChanged() {
+				updateNoDownloadsVisibility()
+			}
+		})
 
 		viewModel.downloadList.observe(viewLifecycleOwner) {
 			lifecycleScope.launchWhenCreated {
 				downloadAdapter.submitData(it)
 			}
 		}
+
+		updateNoDownloadsVisibility()
 
 		return binding.root
 	}
@@ -46,5 +61,9 @@ class DownloadsFragment : Fragment(), DownloadListAdapter.Listener {
 
 	override fun onShowClicked(show: PersistedMediathekShow) {
 		startActivity(MediathekDetailActivity.getStartIntent(context, show.mediathekShow))
+	}
+
+	private fun updateNoDownloadsVisibility() {
+		binding.noDownloads.isVisible = downloadAdapter.itemCount == 0
 	}
 }
