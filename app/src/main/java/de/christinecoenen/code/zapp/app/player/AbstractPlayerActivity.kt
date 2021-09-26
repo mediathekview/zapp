b917f7460d1b9479508eb885a93f49b89a136ca9
Christine Coenen <code.coenen@gmail.com>
2021-09-26 13:13:28 +0200

    Use fullscreen content in a non deprecated way

--- app/src/main/java/de/christinecoenen/code/zapp/app/player/AbstractPlayerActivity.kt
+++ app/src/main/java/de/christinecoenen/code/zapp/app/player/AbstractPlayerActivity.kt
@@ -13,6 +13,8 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.lifecycleScope
 import com.google.android.exoplayer2.ui.StyledPlayerControlView
 import de.christinecoenen.code.zapp.R
@@ -29,6 +31,10 @@ abstract class AbstractPlayerActivity :
 	AppCompatActivity(), StyledPlayerControlView.VisibilityListener {
 
 	private val viewModel: AbstractPlayerActivityViewModel by viewModel()
+
+	private val windowInsetsControllerCompat by lazy {
+		WindowInsetsControllerCompat(window, binding.fullscreenContent)
+	}
 
 	protected lateinit var binding: ActivityAbstractPlayerBinding
 
@@ -273,17 +279,15 @@ abstract class AbstractPlayerActivity :
 
 	private fun showSystemUi() {
 		supportActionBar?.show()
-		binding.fullscreenContent.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
+		windowInsetsControllerCompat.show(WindowInsetsCompat.Type.systemBars())
 	}
 
 	private fun hideSystemUi() {
 		supportActionBar?.hide()
 
-		binding.fullscreenContent.systemUiVisibility = (View.SYSTEM_UI_FLAG_LOW_PROFILE
-			or View.SYSTEM_UI_FLAG_FULLSCREEN
-			or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-			or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-			or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-			or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
+		windowInsetsControllerCompat.hide(WindowInsetsCompat.Type.systemBars())
+		windowInsetsControllerCompat.systemBarsBehavior =
+			WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 	}
 }
