62d22d783b8afee208cdeab0acf186d6256463a9
Christine Coenen <code.coenen@gmail.com>
2021-10-13 14:49:48 +0200

    Fix ui tests

--- app/src/androidTest/java/de/christinecoenen/code/zapp/app/app/livestream/ui/detail/ChannelDetailActivityTest.kt
+++ app/src/androidTest/java/de/christinecoenen/code/zapp/app/app/livestream/ui/detail/ChannelDetailActivityTest.kt
@@ -5,6 +5,7 @@ import android.content.pm.ActivityInfo
 import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import de.christinecoenen.code.zapp.app.livestream.ui.detail.ChannelPlayerActivity
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -15,7 +16,7 @@ class ChannelDetailActivityTest {
 	fun testRecreation() {
 		val context = InstrumentationRegistry.getInstrumentation().targetContext
 
-		val intent = ChannelDetailActivity.getStartIntent(context, "das_erste")
+		val intent = ChannelPlayerActivity.getStartIntent(context, "das_erste")
 		val scenario = ActivityScenario.launch<Activity>(intent)
 
 		scenario.recreate()
