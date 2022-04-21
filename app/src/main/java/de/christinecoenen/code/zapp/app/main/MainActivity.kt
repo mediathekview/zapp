9307a4655989a29d93b83100c69d2613b763513d
Christine Coenen <code.coenen@gmail.com>
2022-04-21 09:59:56 +0200

    Hide bottom navigation for non main destinations

--- app/src/main/java/de/christinecoenen/code/zapp/app/main/MainActivity.kt
+++ app/src/main/java/de/christinecoenen/code/zapp/app/main/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.*
 import androidx.preference.PreferenceManager
@@ -40,9 +42,33 @@ class MainActivity : AppCompatActivity() {
 		setSupportActionBar(binding.toolbar)
 		setupActionBarWithNavController(navController, appBarConfiguration)
 
+		navController.addOnDestinationChangedListener(::onDestinationChanged)
+
 		binding.bottomNavigation.setupWithNavController(navController)
 
 		PreferenceManager.setDefaultValues(application, R.xml.preferences, false)
+	}
+
+	@Suppress("UNUSED_PARAMETER")
+	private fun onDestinationChanged(
+		controller: NavController,
+		destination: NavDestination,
+		arguments: Bundle?
+	) {
+		val isMainDestination = arguments?.getBoolean("is_main_destination", false) == true
+
+		// hide bottom navigation for non main destinations
+		binding.bottomNavigation.isVisible = isMainDestination
+
+		// hide toolbar logo for non main destinations
+		if (isMainDestination) {
+			binding.toolbar.setLogo(R.drawable.ic_zapp_tv_small)
+			binding.toolbar.titleMarginStart =
+				resources.getDimensionPixelSize(R.dimen.toolbar_logo_margin)
+		} else {
+			binding.toolbar.logo = null
+			binding.toolbar.titleMarginStart = 0
+		}
 	}
 
 	override fun onDestroy() {
