ebe2fe9d12117c53798d28870c369c170e4c29ad
Christine Coenen <code.coenen@gmail.com>
2022-04-22 14:36:32 +0200

    Instrumentation test about fragments

---
+++ app/src/androidTest/java/de/christinecoenen/code/zapp/app/about/ui/ChangelogFragmentTest.kt
@@ -0,0 +1,26 @@
+package de.christinecoenen.code.zapp.app.about.ui
+
+import android.content.pm.ActivityInfo
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import de.christinecoenen.code.zapp.R
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ChangelogFragmentTest {
+
+	@Test
+	fun testRecreation() {
+		val scenario = launchFragmentInContainer<ChangelogFragment>(
+			themeResId = R.style.AppTheme
+		)
+
+		// landscape
+		scenario.onFragment {
+			it.activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+		}
+		scenario.recreate()
+	}
+
+}
