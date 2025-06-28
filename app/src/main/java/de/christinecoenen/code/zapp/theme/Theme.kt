a03d990fdf408c5bfa0aa95140c60d9ad301b5a7
Christine Coenen <code.coenen@gmail.com>
2025-06-28 20:49:46 +0200

    Add basic app theme

---
+++ app/src/main/java/de/christinecoenen/code/zapp/theme/Theme.kt
@@ -0,0 +1,133 @@
+package de.christinecoenen.code.zapp.theme
+
+import android.content.res.Configuration
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import de.christinecoenen.code.zapp.app.settings.repository.SettingsRepository
+
+/**
+ * This theme has bee generated with [Material Theme Builder](https://material-foundation.github.io/material-theme-builder/)
+ * with following Parameters:
+ * - Stay true to my color inputs
+ * - Primary: #E1007A
+ * - Secondary: #F44F17
+ * - Tertiary: #B00050
+ * - Error: #FF5449
+ * - Neutral: #F5F5F5
+ * - Neutral Variant: #E3E3E3
+ */
+
+private val lightScheme = lightColorScheme(
+	primary = primaryLight,
+	onPrimary = onPrimaryLight,
+	primaryContainer = primaryContainerLight,
+	onPrimaryContainer = onPrimaryContainerLight,
+	secondary = secondaryLight,
+	onSecondary = onSecondaryLight,
+	secondaryContainer = secondaryContainerLight,
+	onSecondaryContainer = onSecondaryContainerLight,
+	tertiary = tertiaryLight,
+	onTertiary = onTertiaryLight,
+	tertiaryContainer = tertiaryContainerLight,
+	onTertiaryContainer = onTertiaryContainerLight,
+	error = errorLight,
+	onError = onErrorLight,
+	errorContainer = errorContainerLight,
+	onErrorContainer = onErrorContainerLight,
+	background = backgroundLight,
+	onBackground = onBackgroundLight,
+	surface = surfaceLight,
+	onSurface = onSurfaceLight,
+	surfaceVariant = surfaceVariantLight,
+	onSurfaceVariant = onSurfaceVariantLight,
+	outline = outlineLight,
+	outlineVariant = outlineVariantLight,
+	scrim = scrimLight,
+	inverseSurface = inverseSurfaceLight,
+	inverseOnSurface = inverseOnSurfaceLight,
+	inversePrimary = inversePrimaryLight,
+	surfaceDim = surfaceDimLight,
+	surfaceBright = surfaceBrightLight,
+	surfaceContainerLowest = surfaceContainerLowestLight,
+	surfaceContainerLow = surfaceContainerLowLight,
+	surfaceContainer = surfaceContainerLight,
+	surfaceContainerHigh = surfaceContainerHighLight,
+	surfaceContainerHighest = surfaceContainerHighestLight,
+)
+
+private val darkScheme = darkColorScheme(
+	primary = primaryDark,
+	onPrimary = onPrimaryDark,
+	primaryContainer = primaryContainerDark,
+	onPrimaryContainer = onPrimaryContainerDark,
+	secondary = secondaryDark,
+	onSecondary = onSecondaryDark,
+	secondaryContainer = secondaryContainerDark,
+	onSecondaryContainer = onSecondaryContainerDark,
+	tertiary = tertiaryDark,
+	onTertiary = onTertiaryDark,
+	tertiaryContainer = tertiaryContainerDark,
+	onTertiaryContainer = onTertiaryContainerDark,
+	error = errorDark,
+	onError = onErrorDark,
+	errorContainer = errorContainerDark,
+	onErrorContainer = onErrorContainerDark,
+	background = backgroundDark,
+	onBackground = onBackgroundDark,
+	surface = surfaceDark,
+	onSurface = onSurfaceDark,
+	surfaceVariant = surfaceVariantDark,
+	onSurfaceVariant = onSurfaceVariantDark,
+	outline = outlineDark,
+	outlineVariant = outlineVariantDark,
+	scrim = scrimDark,
+	inverseSurface = inverseSurfaceDark,
+	inverseOnSurface = inverseOnSurfaceDark,
+	inversePrimary = inversePrimaryDark,
+	surfaceDim = surfaceDimDark,
+	surfaceBright = surfaceBrightDark,
+	surfaceContainerLowest = surfaceContainerLowestDark,
+	surfaceContainerLow = surfaceContainerLowDark,
+	surfaceContainer = surfaceContainerDark,
+	surfaceContainerHigh = surfaceContainerHighDark,
+	surfaceContainerHighest = surfaceContainerHighestDark,
+)
+
+@Composable
+fun AppTheme(
+	darkTheme: Boolean = isSystemInDarkTheme(),
+	content: @Composable () -> Unit
+) {
+	val settingsRepository = SettingsRepository(LocalContext.current)
+	val dynamicColor = settingsRepository.dynamicColors
+
+	val colorScheme = when {
+		dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+			val context = LocalContext.current
+			if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+		}
+
+		darkTheme -> darkScheme
+		else -> lightScheme
+	}
+
+	MaterialTheme(
+		colorScheme = colorScheme,
+		typography = AppTypography,
+		content = content
+	)
+}
+
+@Preview(name = "Dark Mode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(name = "Light Mode", showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_NO)
+annotation class ThemePreviews
