426303ffaf979ee48e8e019ee387277b6477adc9
Christine Coenen <code.coenen@gmail.com>
2025-06-28 20:57:51 +0200

    Remove unused imports

--- app/src/main/java/de/christinecoenen/code/zapp/theme/Theme.kt
+++ app/src/main/java/de/christinecoenen/code/zapp/theme/Theme.kt
@@ -9,8 +9,6 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Immutable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import de.christinecoenen.code.zapp.app.settings.repository.SettingsRepository
