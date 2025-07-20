6ca43dacac1b56a1319e1cba96b2920a33c175b1
Christine Coenen <code.coenen@gmail.com>
2025-07-20 12:01:25 +0200

    Add search bar

--- app/src/main/java/de/christinecoenen/code/zapp/tv2/mediathek/MediaCenterScreen.kt
+++ app/src/main/java/de/christinecoenen/code/zapp/tv2/mediathek/MediaCenterScreen.kt
@@ -1,11 +1,14 @@
 package de.christinecoenen.code.zapp.tv2.mediathek
 
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -32,30 +35,33 @@ fun MediaCenterScreen(
 	val showFlow = remember { viewModel.shows }
 	val shows = showFlow.collectAsLazyPagingItems()
 	var selectedShowIndex by rememberSaveable { mutableStateOf<Int?>(null) }
-	val selectedShow by remember {
+	/*val selectedShow by remember {
 		derivedStateOf { if (selectedShowIndex == null) null else shows[selectedShowIndex!!] }
+	}*/
+	var searchText by rememberSaveable { mutableStateOf("") }
+
+	LaunchedEffect(searchText) {
+		viewModel.loadShows(searchText)
 	}
 
-	Row(
+	Column(
 		modifier = Modifier
-            .padding(horizontal = 58.dp)
-            .padding(top = 104.dp)
+			.padding(horizontal = 202.dp)
+			.padding(top = 104.dp)
 	) {
+		// TODO: style
+		TextField(
+			value = searchText,
+			onValueChange = { searchText = it },
+			singleLine = true,
+			modifier = Modifier.fillMaxWidth()
+		)
+		Spacer(Modifier.height(20.dp))
 		ShowList(
 			showList = shows,
 			selectedShowIndex = selectedShowIndex,
 			onShowClick = { selectedShowIndex = it },
 			modifier = Modifier.weight(0.5f)
 		)
-
-		selectedShow?.let {
-			Spacer(Modifier.width(20.dp))
-
-			ShowDetails(
-				topic = it.topic,
-				title = it.title,
-				modifier = Modifier.weight(0.5f)
-			)
-		}
 	}
 }
